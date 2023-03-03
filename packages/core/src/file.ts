952d23013571fdf5ab9b98f5cb7b23c8efd4d802
masb0ymas <n.fajri@outlook.com>
2023-03-03 14:29:54 +0700

    feat: add function read html file

--- packages/core/src/file.ts
+++ packages/core/src/file.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import * as fsAsync from 'fs/promises'
 import path from 'path'
 
 /**
@@ -9,5 +10,19 @@ export function createDirNotExist(_path: string): void {
   if (!fs.existsSync(path.resolve(_path))) {
     fs.mkdirSync(_path, { recursive: true })
     console.log(`create directory ${_path}`)
+  }
+}
+
+/**
+ * Read HTML File
+ * @param _path
+ * @returns
+ */
+export async function readHTMLFile(_path: string): Promise<string> {
+  try {
+    const result = await fsAsync.readFile(_path, { encoding: 'utf-8' })
+    return result
+  } catch (err) {
+    throw new Error('invalid html path')
   }
 }
