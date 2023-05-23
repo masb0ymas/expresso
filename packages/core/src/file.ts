0b03bb475a10bb69f46ef35eca6dded20f8d34b5
masb0ymas <n.fajri@outlook.com>
2023-05-23 14:00:58 +0700

    fix: file helper

--- packages/core/src/file.ts
+++ packages/core/src/file.ts
@@ -5,27 +5,27 @@ import { printLog } from './formatter'
 
 /**
  * Create Dir Not Exist
- * @param _path
+ * @param filePath
  */
-export function createDirNotExist(_path: string): void {
-  if (!fs.existsSync(path.resolve(_path))) {
-    fs.mkdirSync(_path, { recursive: true })
+export function createDirNotExist(filePath: string): void {
+  if (!fs.existsSync(path.resolve(filePath))) {
+    fs.mkdirSync(filePath, { recursive: true })
 
-    const logMessage = printLog('FileSystem :', `create directory ${_path}`)
+    const logMessage = printLog('FileSystem :', `create directory ${filePath}`)
     console.log(logMessage)
   }
 }
 
 /**
  * Read HTML File
- * @param _path
+ * @param filePath
  * @returns
  */
 export async function readHTMLFile(
-  _path: fs.PathLike | fs.promises.FileHandle
+  filePath: fs.PathLike | fs.promises.FileHandle
 ): Promise<string> {
   try {
-    const result = await fsAsync.readFile(_path, { encoding: 'utf-8' })
+    const result = await fsAsync.readFile(filePath, { encoding: 'utf-8' })
     return result
   } catch (err) {
     throw new Error('invalid html path')
@@ -34,23 +34,23 @@ export async function readHTMLFile(
 
 /**
  *
- * @param _path
+ * @param filePath
  * @param streamFile
  */
 export async function writeFileStream(
-  _path: string,
+  filePath: string,
   streamFile: Buffer
 ): Promise<void> {
   try {
-    await fsAsync.writeFile(_path, streamFile)
+    await fsAsync.writeFile(filePath, streamFile)
 
-    const msgType = `File : ${_path}`
+    const msgType = `File : ${filePath}`
     const message = 'generate file successfully'
     const logMessage = printLog(msgType, message)
 
     console.log(logMessage)
   } catch (err) {
-    const msgType = `File : ${_path}`
+    const msgType = `File : ${filePath}`
     const logMessage = printLog(msgType, String(err), { label: 'error' })
 
     console.log(logMessage)
@@ -60,20 +60,20 @@ export async function writeFileStream(
 
 /**
  * Delete File
- * @param _path
+ * @param filePath
  */
-export function deleteFile(_path: string): void {
-  const filePath = path.resolve(_path)
+export function deleteFile(filePath: string): void {
+  const _path = path.resolve(filePath)
 
   const msgType = `File : ${filePath}`
 
-  if (_path && fs.existsSync(filePath)) {
+  if (_path && fs.existsSync(_path)) {
     const message = 'has been deleted'
     const logMessage = printLog(msgType, message)
 
     console.log(logMessage)
 
-    fs.unlinkSync(path.resolve(filePath))
+    fs.unlinkSync(_path)
   } else {
     const message = 'not exist'
     const logMessage = printLog(msgType, message, { label: 'error' })
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 export * as currency from './currency'
 export * from './file'
 export * from './formatter'
+export * from './phone'
 export * as randomString from './randomString'
